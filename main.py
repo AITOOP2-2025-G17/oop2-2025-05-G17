dc4feb2af0b006833fd0be989b28eca9b201ff69
samurai-7xy <108717711+samurai-7xy@users.noreply.github.com>
2025-10-30 16:00:15 +0900

    Revert "Merge branch 'main' of https://github.com/AITOOP2-2025-G17/oop2-2025-05-G17 into Okazawa05"
    This reverts commit 7757136c34be7b5e9352239aa6a4c118bb33ba65, reversing
    changes made to 2d8ab8e56410c072ccadd5e58c4b90ca3cff18ce.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+<<<<<<< HEAD
 from src.k24036 import k24036
-from src.k24019 import k24019
-from src.k24122 import k24122
+
 if __name__ == "__main__":
-    k24019()
-    k24122()
     k24036()
+=======
+from src.k24122 import k24122
+
+if __name__ == "__main__":
+    k24122()
+>>>>>>> a6429a61c42ceb36341a72d7a74dad211094d4b8
--- src/lecture05_01.py
+++ src/lecture05_01.py
@@ -10,9 +10,11 @@ def lecture05_01():
     app.run()
 
     # 画像をローカル変数に保存
-    google_img : cv2.Mat = cv2.imread('images/google.png')
-    # capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
-    capture_img : cv2.Mat = app.get_img()  # カメラからキャプチャした画像を取得
+    google_img: cv2.Mat = cv2.imread("images/google.png")
+    capture_img: cv2.Mat = cv2.imread(
+        "images/camera_capture.png"
+    )  # 動作テスト用なので提出時にこの行を消すこと
+    # capture_img : cv2.Mat = "implement me"
 
     g_hight, g_width, g_channel = google_img.shape
     c_hight, c_width, c_channel = capture_img.shape
@@ -21,16 +23,11 @@ def lecture05_01():
 
     for x in range(g_width):
         for y in range(g_hight):
-            b, g, r = google_img[y, x]
+            g, b, r = google_img[y, x]
             # もし白色(255,255,255)だったら置き換える
             if (b, g, r) == (255, 255, 255):
-                # カメラ画像を(0,0)からグリッド状に並べる
-                # カメラ画像のどの位置の画素を使うかを計算
-                cam_x = x % c_width
-                cam_y = y % c_hight
-                # google画像の白い部分をカメラ画像で置き換え
-                google_img[y, x] = capture_img[cam_y, cam_x]
+                pass
+                # implement me
 
     # 書き込み処理
-    cv2.imwrite('output_images/lecture05_01_K24019.png', google_img)
-    print("画像を保存しました: output_images/lecture05_01_K24019.png")
+    # implement me
