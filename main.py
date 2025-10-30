5fd2dd5e62446fdb6cf86540a109035bda705a05
samurai-7xy <108717711+samurai-7xy@users.noreply.github.com>
2025-10-30 16:00:18 +0900

    Revert "K24036"
    This reverts commit 2d8ab8e56410c072ccadd5e58c4b90ca3cff18ce, reversing
    changes made to 86173808cda73009a119f14dbbfba7361928aae0.

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from src.k24036 import k24036
 
 if __name__ == "__main__":
     k24036()
-=======
-from src.k24122 import k24122
-
-if __name__ == "__main__":
-    k24122()
->>>>>>> a6429a61c42ceb36341a72d7a74dad211094d4b8
